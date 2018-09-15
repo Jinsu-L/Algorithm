f09acb4e80af5d4092e646c7ef7bddfdc573046a
jinsu <dla0830ld@gmail.com>
2018-09-15 22:33:57 +0900

    programmers 문자열을 정수로 바꾸기

---
+++ Programmers/Level1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = 0
+
+    for idx, number in enumerate(s[::-1]):
+        if number == '-':
+            answer *= -1
+        else:
+            answer += int(number) * (10 ** idx)
+    return answer
+
+print(solution("-1234"))
