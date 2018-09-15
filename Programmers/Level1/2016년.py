2d837c1897e9fb370a21e99328542b62d6f1b8fd
jinsu <dla0830ld@gmail.com>
2018-09-15 20:55:48 +0900

    programmers 2016년 개선

--- Programmers/Level1/2016년.py
+++ Programmers/Level1/2016년.py
@@ -2,9 +2,7 @@ def solution(a, b):
     week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    day = sum([m for i, m in enumerate(month) if i < a - 1])
-    day += b
-
+    day = sum(month[:a-1]) + b
     answer = week[day % 7]
     return answer
 
